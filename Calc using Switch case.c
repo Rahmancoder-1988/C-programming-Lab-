c1a4515d0cdce9ec9fe3662a44fe3977efba5ab7
Rahmancoder-1988 <kkhanrahman1988@eng.rizvi.edu.in>
2025-10-13 10:17:27 +0530

    Create Calc using Switch case.c

---
+++ Calc using Switch case.c
@@ -0,0 +1,30 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+    int num1=5,num2=15;
+    int choice;
+    
+    printf("1.addition\n2.subtraction\n3.multiplication\n4.division\n5.exit\nEnter your choice => ");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+        case 1:
+    printf("addition is %d",num1+num2);
+    break;
+    case 2:
+    printf("subtraction is %d",num1-num2);
+    break;
+    case 3:
+    printf("multiplication is %d",num1*num2);
+    break;
+    case 4:
+    printf("division is %f",(float)num1/num2);
+    case 5:
+    break;
+    default:
+    printf("invalid choice");
+    break;}
+   
+    return 0;}
