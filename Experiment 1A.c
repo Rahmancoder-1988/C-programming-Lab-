9e51375a6df14836117143de35307b459a5cc498
Rahmancoder-1988 <kkhanrahman1988@eng.rizvi.edu.in>
2025-10-14 19:41:05 +0530

    Create Experiment 1A.c

---
+++ Experiment 1A.c
@@ -0,0 +1,14 @@
+/*Name: Mohd Rahman Khan
+Class: Mechanical       Div.:F
+UIN:251M004*/
+#include <stdio.h>
+
+int main() {
+  printf("Name\t:Mohd Rahman Khan\n");
+  printf("Age\t:18\n");
+  printf("Class\t:Mechanical\n");
+  printf("Div.\t:F\n");
+  printf("UIN\t:251M004\n");
+
+    return 0;
+}
