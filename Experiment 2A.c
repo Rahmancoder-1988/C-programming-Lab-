53ef9ffb3e346db797ce848d8e3a406940188dba
Rahmancoder-1988 <kkhanrahman1988@eng.rizvi.edu.in>
2025-10-31 19:42:48 +0530

    Create Experiment 2A.c

---
+++ Experiment 2A.c
@@ -0,0 +1,15 @@
+/* Name:Mohd.Rahman Khan
+Class:Mechanical      Div.:F
+UIN:251M004 */
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter the number");
+    scanf("%d", &num);
+    if (num%2==0);
+    {
+        printf("The number is even");
+    }
+    return 0;
+}
