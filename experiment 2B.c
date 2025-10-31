5ddd06f736f4e0819163ccd0552b63642d803ff5
Rahmancoder-1988 <kkhanrahman1988@eng.rizvi.edu.in>
2025-10-31 19:16:37 +0530

    Create experiment 2B.c

---
+++ experiment 2B.c
@@ -0,0 +1,23 @@
+// Online C compiler to run C program online
+
+#include <stdio.h>
+/* Name:Mohd.Rahman Khan
+Class:Mechanical      Div.:F
+UIN:251M004 */
+    int main() {
+    int num1, num2, i, sum = 0;
+    printf("enter the first number");
+    scanf("%d", &num1);
+    printf("enter the second number");
+    scanf("%d",&num2);
+    if (num1>num2)
+    {
+    int temp = num1;
+    num1=num2;
+    num2=temp;
+    }
+    for(i = num1 + 1;i < num2; i++)
+       { sum+= i;}
+       printf("sum of all the numbers between %d and %d is: %d\n", num1, num2, sum);
+    return 0;
+}
